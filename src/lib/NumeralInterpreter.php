9148cdd62b315ca51247503ad30180095b43bb27
Wes Crow <wes@dynamit.com>
2016-07-24 13:53:39 -0400

    When NumeralInterpreter is asked to interpret an ArabicNumeral to a RomanNumeral, an ArabicNumeral must be the input.

---
+++ src/lib/NumeralInterpreter.php
@@ -0,0 +1,14 @@
+<?php
+
+	require_once('ArabicNumeral.php');
+	require_once('RomanNumeral.php');
+
+	/**
+	 * Class NumeralInterpreter
+	 */
+	class NumeralInterpreter {
+
+		public function arabicToRoman(ArabicNumeral $arabic) {
+
+		}
+	}
---
+++ tests/NumeralInterpreterTest.php
@@ -0,0 +1,25 @@
+<?php
+
+	require_once(__dir__ . '../../src/lib/NumeralInterpreter.php');
+
+	/**
+	 * Class NumeralInterpreter
+	 * Class for testing the NumeralInterpreter library.
+	 */
+	class NumeralInterpreterTest extends \PHPUnit_Framework_TestCase {
+
+		public function testWhenInterpreterInterpretsArabicToRomanInvalidArabicTypeExceptionThrown() {
+			$romanNumeral = new RomanNumeral();
+			$numeralInterpreter = new NumeralInterpreter();
+			$exceptionMessage = 'No exception thrown.';
+			$isException = false;
+			try {
+				$romanNumeral->setValue('I');
+				$numeralInterpreter->arabicToRoman($roman);
+			} catch (Exception $e) {
+				$isException = true;
+				$exceptionMessage = $e->getMessage();
+			}
+			$this->assertEquals(true, $isException, $exceptionMessage);
+		}
+	}
